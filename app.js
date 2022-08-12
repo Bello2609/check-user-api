2213d116a7a6a6a6a46b722314659767d9abc9c1
bello hadi <bellohadi82@gmail.com>
2022-08-12 11:17:47 +0100

    added mongodb url

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require();
+const cors = require("cors");
 const checkRoute = require("./route/check");
 require("dotenv").config();
 
@@ -14,7 +14,7 @@ app.use(bodyparser.json());
 app.use(cors());
 
 app.use(checkRoute);
-mongoose.connect("your mongodb url").then(result=>{
+mongoose.connect("mongodb+srv://proctor_test:XGlC9xvWLc1S0pyP@cluster0.buffyyq.mongodb.net/?retryWrites=true&w=majority").then(result=>{
     app.listen(PORT, ()=>{
         console.log(`the server is running at port ${PORT}`);
     });
